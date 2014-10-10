b4d0e7188162caba15d2ee078e496c988734ef62
Dmitri Popov <dmpop@linux.com>
2014-10-10 11:26:37 +0200

    dn2820fykh-kubuntu

---
+++ install_pandoc-pubkit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get install pandoc git
+git clone https://github.com/dmpop/pandoc-pubkit.git
+#Install Calibre
+sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
